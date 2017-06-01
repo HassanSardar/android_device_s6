8d8686f02e2dc587b8649cf3429059f9fb4e43e4
HassanSardar <hassansardar08@live.com>
2017-06-01 18:28:48 +0000

    We Live In Lineage Now!

--- lineage.mk
+++ lineage.mk
@@ -7,11 +7,11 @@ PRODUCT_RELEASE_NAME := s6
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/infinix/x510/device_x510.mk)
+$(call inherit-product, device/qmobile/s6/device_s6.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s6
-PRODUCT_NAME := cm_s6
+PRODUCT_NAME := lineage_s6
 PRODUCT_BRAND := QMOBILE
 PRODUCT_MODEL := S6
 PRODUCT_MANUFACTURER := QMOBILE
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,5 +21,5 @@
 # function: add_lunch_combo generic-eng
 
 for var in eng user userdebug; do
-  add_lunch_combo cm_s6-$var
+  add_lunch_combo lineage_s6-$var
 done
